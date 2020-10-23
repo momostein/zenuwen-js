10b4b09c236d67cf6bb1a09e092cfc213650c367
Sander Ouderits <sanderouderits@hotmail.com>
2020-10-23 18:21:47 +0200

    dynamische dropzone

--- src/js/stapel.js
+++ src/js/stapel.js
@@ -1,41 +1,53 @@
 import Phaser from 'phaser';
 
-export class Stapel extends Phaser.GameObjects.Zone{
-    constructor(scene, x,y,width,height){
-        super(scene, x, y, width, height);
-        scene.add.existing(this);
-        this.setRectangleDropZone(width, height);
-        this.cards = []; 
-        this.dropZoneOutline = scene.add.graphics();
-        this.dropZoneOutline.lineStyle(4,0xff0000);
-        this.dropZoneOutline.strokeRect(x-width/2, y-height/2,width,height);
-    }
+export class Stapel extends Phaser.GameObjects.Zone {
+	constructor (scene, x, y, width, height) {
+		super(scene, x, y, width, height);
+		scene.add.existing(this);
+		this.setRectangleDropZone(width, height);
+		this.cards = [];
+		this.border = scene.add.rectangle(this.x, this.y, this.width, this.height).setFillStyle().setStrokeStyle(5, 0xff0000, 1);
+	}
 
-    addCard(card){
-        if(this.getNumberOfCards()!=0){
-            this.cards[this.getNumberOfCards()-1].disableInteractive();
-        }
-        this.cards.push(card);
-        this.dropZoneOutline.setPosition( this.dropZoneOutline.x , this.dropZoneOutline.y+20)
-        this.setPosition( this.x , this.y+20)
-    }
+	addCard (card) {
+		if (this.getNumberOfCards() != 0) {
+			this.cards[this.getNumberOfCards() - 1].disableInteractive();
+			this.setPosition(this.x, this.y + 10);
+			this.setSize(this.width, this.height + 20);
+			this.setDisplaySize(this.width, this.height + 20);
+		} else {
+			this.setPosition(this.x, this.y);
+			this.setSize(this.width, this.height);
+			this.setDisplaySize(this.width, this.height);
+		}
 
-    popCard(){
-        this.cards.pop();
-        if(this.getNumberOfCards()!=0){
-            this.cards[this.getNumberOfCards()-1].setInteractive();
-        }
-        this.setPosition( this.x , this.y-20)
-        this.dropZoneOutline.setPosition( this.dropZoneOutline.x , this.dropZoneOutline.y-20)
+		this.border.setPosition(this.x, this.y);
+		this.border.setDisplaySize(this.width, this.height);
+		this.cards.push(card);
+	}
 
+	popCard () {
+		if (this.getNumberOfCards() >= 2) {
+			this.cards[this.getNumberOfCards() - 2].setInteractive();
+			this.setPosition(this.x, this.y - 10);
+			this.setSize(this.width, this.height - 20);
+			this.setDisplaySize(this.width, this.height - 20);
+		} else {
+			this.setPosition(this.x, this.y);
+			this.setSize(this.width, this.height);
+			this.setDisplaySize(this.width, this.height);
+		}
 
-    }
+		this.border.setPosition(this.x, this.y);
+		this.border.setDisplaySize(this.width, this.height);
+		this.cards.pop();
+	}
 
-    getNumberOfCards(){
-        return this.cards.length;
-    }
+	getNumberOfCards () {
+		return this.cards.length;
+	}
 
-    containsCard(card){
-        return (this.cards.includes(card));
-    }
-};
+	containsCard (card) {
+		return (this.cards.includes(card));
+	}
+}
