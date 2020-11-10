3b8f530d05d9c0c471013aa9bc9da164996596fc
Mathias Van den Broek <mathiasvdb1998@gmail.com>
2020-11-10 10:37:22 +0100

    Kenney kleurenpallet

--- src/js/style/index.js
+++ src/js/style/index.js
@@ -4,18 +4,41 @@ export const style = {
 	colors: {
 		primary: Display.Color.HexStringToColor('#04B2D9'),
 		secondary: Display.Color.HexStringToColor('#F2360C'),
-		background: Display.Color.HexStringToColor('#ffef9e'),
-		textHover:	Display.Color.HexStringToColor('#F2CB05'),
-		textColor:	Display.Color.HexStringToColor('#D98E04'),
+		background: Display.Color.HexStringToColor('#3f7cb6'),
+		textHover:	Display.Color.HexStringToColor('#949494'),
+		textColor:	Display.Color.HexStringToColor('#FFF'),
 	},
 };
 
 /*
 Kleurenpallet:
 
-- Blauw: #4485bf
-- Oranje: #C93F3F
-- Geel_licht: #F2CB05
-- Geel_midden: #F2B705
-- Geel_donker: #D98E04
+#3f7cb6
+#5ad2f0
+#1ea7e1
+#19688a
+
+#e86a17
+#d46217
+#9a4c18
+
+#ffcc00
+#f0c100
+#a18100
+
+#a03232
+#802828
+
+#9d74de
+#8c61d0
+#624098
+
+#73cd4b
+#70bd4e
+#4c853a
+
+#2a2a2a
+#434343
+#949494
+#bbbbbb
 */
