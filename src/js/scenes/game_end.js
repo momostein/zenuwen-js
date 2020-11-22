814587bac640abb9a7ec5a5461f4f40203e52fd5
Mathias Van den Broek <mathiasvdb1998@gmail.com>
2020-11-22 15:34:21 +0100

    Main menu

--- src/js/scenes/game_end.js
+++ src/js/scenes/game_end.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
-import { style } from '../style';
 import { TextButton } from '../button';
+import { style } from '../style';
+
 export default class GameEnd extends Phaser.Scene {
 	constructor () {
 		super('gameEnd'); // id of Scene
@@ -12,6 +13,6 @@ export default class GameEnd extends Phaser.Scene {
 	create () {
 		const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2; // x for auto center
 		this.add.text(20, 20, 'Game End', { fontFamily: 'lemonMilk' }).setColor(style.colors.textColor.rgba);
-		this.mainMenu = new TextButton(this, screenCenterX, 700, 230, 100, 'Menu', 30, undefined, undefined, () => this.scene.start('mainMenu'));
+		this.mainMenu = new TextButton(this, screenCenterX, 700, 230, 100, 'Menu', 30, 6, undefined, undefined, () => this.scene.start('mainMenu'));
 	}
 }
--- src/js/scenes/pause_menu.js
+++ src/js/scenes/pause_menu.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
-import { style } from '../style';
 import { TextButton } from '../button';
+import { style } from '../style';
+
 export default class PauseMenu extends Phaser.Scene {
 	constructor () {
 		super('pauseMenu'); // id of Scene
@@ -12,6 +13,6 @@ export default class PauseMenu extends Phaser.Scene {
 	create () {
 		const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2; // x for auto center
 		this.add.text(20, 20, 'Pause Menu', { fontFamily: 'lemonMilk' }).setColor(style.colors.textColor.rgba);
-		this.mainMenu = new TextButton(this, screenCenterX, 700, 230, 100, 'continue', 30, undefined, undefined, () => this.scene.switch('game'));
+		this.mainMenu = new TextButton(this, screenCenterX, 700, 230, 100, 'continue', 30, 6, undefined, undefined, () => this.scene.switch('game'));
 	}
 }
